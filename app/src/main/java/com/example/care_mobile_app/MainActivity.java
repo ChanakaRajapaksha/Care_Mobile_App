162ea79fc0dcda64e974143b1250b31622b1850b
RCP Rajapaksha <it20137250@my.sliit.lk>
2022-05-05 09:48:48 +0530

    add dev branch

--- app/src/main/java/com/example/care_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/care_mobile_app/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    //initial comment
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
