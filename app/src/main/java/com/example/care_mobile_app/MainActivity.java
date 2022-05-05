81f2d287cc0618965f97530abbbd866800e807a2
RCP Rajapaksha <it20137250@my.sliit.lk>
2022-05-05 09:28:59 +0530

    update commit

--- app/src/main/java/com/example/care_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/care_mobile_app/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //initial comment
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
